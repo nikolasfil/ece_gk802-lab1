a5075fddf35301a349f2a2f76468868b69a14608
nikolasfil <nikolas13in@gmail.com>
2023-02-20 12:15:51 +0200

    First exercise

--- ex0.py
+++ ex0.py
@@ -1,19 +1,32 @@
-import requests  # εισαγωγή της βιβλιοθήκης
+#!/bin/python
+import requests
+import datetime
+
+# 1.
+# url = input("URL: ")
+url = "http://python.org/"
+# url = "https://www.youtube.com"
 
 
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
-
-
-url = 'http://python.org/'  # προσδιορισμός του url
-
+# 2.
+# Πραγματοποιηση αιτηματος 
 with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+    # 3.
+    headers = response.headers
+    # more(headers)
+    print(f'Headers: {headers}')
+
+    # 4.
+    print(f'\nSoftware of the server: {headers["Server"]}\n')
+
+    if "set-cookie" in headers.keys():
+        # cookies_keys = response.cookies.keys
+        cookies = response.cookies
+        for cookie in cookies:
+            print(f"\nThe site uses cookie.\nName: {cookie.name}\nValue: {cookie.value}\nExpiration Date: {datetime.datetime.fromtimestamp(float(cookie.expires)) if cookie.expires is not None else 'Does Not Expire'}")        
+    else:
+        print('\nThe site does not use cookies')
+
+
+
+
